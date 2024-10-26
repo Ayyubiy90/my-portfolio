5402869e9bf675f9f06b4ee53ef81b427b43bf76
AYB <ayyubiy67@gmail.com>
2024-10-26 10:05:48 +0100

    Import react hooks

---
+++ src/components/Contact.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { Mail, Phone, LocationMarker } from "lucide-react";
+
